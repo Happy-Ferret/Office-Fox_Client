4de2069b5e64466f5abbde599451c2d11a281623
root <root@Virtasia.Handwerkskammer.de>
2014-11-22 19:02:54 +0100

    Basic folder hierarchy.

---
+++ defaults/preferences/Office-Fox.js
@@ -0,0 +1,7 @@
+pref("toolkit.defaultChromeURI", "chrome://Office-Fox/content/main.xul");
+pref("general.useragent.extra.Office-Fox", "Office-Fox/0.8.0");
+pref("browser.dom.window.dump.enabled", false);
+pref("javascript.options.showInConsole", false);
+pref("javascript.options.strict", false);
+pref("nglayout.debug.disable_xul_cache", false);
+pref("nglayout.debug.disable_xul_fastload", false);
